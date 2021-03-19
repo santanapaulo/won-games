071c21c4cf088858fa6c0464f046f12236a591c8
Paulo Santana (PH) <santana.pauloh@gmail.com>
2021-03-19 20:15:44 -0300

    huge font size in Heading

--- src/components/Heading/index.test.tsx
+++ src/components/Heading/index.test.tsx
@@ -78,4 +78,12 @@ describe('<Heading />', () => {
       modifier: '::after',
     });
   });
+
+  it('should render a heading with a huge size', () => {
+    renderWithTheme(<Heading size="huge">Won Games</Heading>);
+
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
+      'font-size': '5.2rem',
+    });
+  });
 });
