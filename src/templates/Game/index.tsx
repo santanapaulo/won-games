4aa63375249caee47a8efc939f2375a949cb7f9d
Paulo Santana (PH) <santana.pauloh@gmail.com>
2021-05-02 20:17:42 -0300

    Game template

---
+++ src/templates/Game/index.tsx
@@ -0,0 +1,48 @@
+import Base from 'templates/Base';
+
+import GameInfo, { GameInfoProps } from 'components/GameInfo';
+import Gallery, { GalleryImageProps } from 'components/Gallery';
+import GameDetails, { GameDetailsProps } from 'components/GameDetails';
+
+import * as S from './styles';
+import TextContent from 'components/TextContent';
+
+export type GameTemplateProps = {
+  cover: string;
+  gameInfo: GameInfoProps;
+  gallery?: GalleryImageProps[];
+  description: string;
+  details: GameDetailsProps;
+};
+
+const Game = ({
+  cover,
+  gameInfo,
+  gallery,
+  description,
+  details,
+}: GameTemplateProps) => (
+  <Base>
+    <S.Cover src={cover} role="image" aria-label="cover" />
+
+    <S.Main>
+      <S.SectionGameInfo>
+        <GameInfo {...gameInfo} />
+      </S.SectionGameInfo>
+
+      <S.SectionGallery>
+        {!!gallery && <Gallery items={gallery} />}
+      </S.SectionGallery>
+
+      <S.SectionDescription>
+        <TextContent title="Description" content={description} />
+      </S.SectionDescription>
+
+      <S.SectionGameDetails>
+        <GameDetails {...details} />
+      </S.SectionGameDetails>
+    </S.Main>
+  </Base>
+);
+
+export default Game;
