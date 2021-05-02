1fed41f45467f7a497e243bb506d9b131c3f46b7
Paulo Santana (PH) <santana.pauloh@gmail.com>
2021-05-02 20:32:26 -0300

    template games testing

--- src/templates/Game/index.tsx
+++ src/templates/Game/index.tsx
@@ -3,9 +3,12 @@ import Base from 'templates/Base';
 import GameInfo, { GameInfoProps } from 'components/GameInfo';
 import Gallery, { GalleryImageProps } from 'components/Gallery';
 import GameDetails, { GameDetailsProps } from 'components/GameDetails';
+import TextContent from 'components/TextContent';
+import Showcase from 'components/Showcase';
+import { GameCardProps } from 'components/GameCard';
+import { HighlightProps } from 'components/Highlight';
 
 import * as S from './styles';
-import TextContent from 'components/TextContent';
 
 export type GameTemplateProps = {
   cover: string;
@@ -13,6 +16,9 @@ export type GameTemplateProps = {
   gallery?: GalleryImageProps[];
   description: string;
   details: GameDetailsProps;
+  upcomingGames: GameCardProps[];
+  upcomingHighlight: HighlightProps;
+  recommendedGames: GameCardProps[];
 };
 
 const Game = ({
@@ -21,6 +27,9 @@ const Game = ({
   gallery,
   description,
   details,
+  upcomingGames,
+  upcomingHighlight,
+  recommendedGames,
 }: GameTemplateProps) => (
   <Base>
     <S.Cover src={cover} role="image" aria-label="cover" />
@@ -41,6 +50,14 @@ const Game = ({
       <S.SectionGameDetails>
         <GameDetails {...details} />
       </S.SectionGameDetails>
+
+      <Showcase
+        title="Upcoming"
+        games={upcomingGames}
+        highlight={upcomingHighlight}
+      />
+
+      <Showcase title="You may like these games" games={recommendedGames} />
     </S.Main>
   </Base>
 );
