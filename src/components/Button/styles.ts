902207a73554dc6b3098e4790105080587d328b6
Paulo Santana (PH) <paulo.santana@xpi.com.br>
2021-05-13 17:36:59 -0300

    PaymentOptions e Cart page

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -28,6 +28,7 @@ const wrapperModifiers = {
   withIcon: (theme: DefaultTheme) => css`
     svg {
       width: 1.5rem;
+
       & + span {
         margin-left: ${theme.spacings.xxsmall};
       }
@@ -36,14 +37,21 @@ const wrapperModifiers = {
   minimal: (theme: DefaultTheme) => css`
     background: none;
     color: ${theme.colors.primary};
+
     &:hover {
       color: ${darken(0.1, theme.colors.primary)};
+    }
+  `,
+  disabled: () => css`
+    &:disabled {
+      cursor: not-allowed;
+      filter: saturate(30%);
     }
   `,
 };
 
 export const Wrapper = styled.button<WrapperProps>`
-  ${({ theme, size, fullWidth, hasIcon, minimal }) => css`
+  ${({ theme, size, fullWidth, hasIcon, minimal, disabled }) => css`
     display: inline-flex;
     align-items: center;
     justify-content: center;
@@ -55,14 +63,17 @@ export const Wrapper = styled.button<WrapperProps>`
     border-radius: ${theme.border.radius};
     padding: ${theme.spacings.xxsmall};
     text-decoration: none;
+
     &:hover {
       background: ${minimal
         ? 'none'
         : `linear-gradient(180deg, #e35565 0%, #d958a6 50%)`};
     }
+
     ${!!size && wrapperModifiers[size](theme)};
     ${!!fullWidth && wrapperModifiers.fullWidth()};
     ${!!hasIcon && wrapperModifiers.withIcon(theme)};
     ${!!minimal && wrapperModifiers.minimal(theme)};
+    ${disabled && wrapperModifiers.disabled()};
   `}
 `;
