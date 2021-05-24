50ed95b186af9112c2ef7072d93e1bd5c6227d1e
Paulo Santana (PH) <paulo.santana@xpi.com.br>
2021-05-24 10:25:23 -0300

    Refac TextField e feat ProfileMenu

--- src/components/TextField/index.tsx
+++ src/components/TextField/index.tsx
@@ -5,7 +5,6 @@ import * as S from './styles';
 export type TextFieldProps = {
   onInput?: (value: string) => void;
   label?: string;
-  labelFor?: string;
   initialValue?: string;
   icon?: React.ReactNode;
   iconPosition?: 'left' | 'right';
@@ -17,12 +16,12 @@ const TextField = ({
   icon,
   iconPosition = 'left',
   label,
-  labelFor = '',
+  name,
   initialValue = '',
   error,
   disabled = false,
   onInput,
-  ...rest
+  ...props
 }: TextFieldProps) => {
   const [value, setValue] = useState(initialValue);
 
@@ -35,7 +34,7 @@ const TextField = ({
 
   return (
     <S.Wrapper disabled={disabled} error={!!error}>
-      {!!label && <S.Label htmlFor={labelFor}>{label}</S.Label>}
+      {!!label && <S.Label htmlFor={name}>{label}</S.Label>}
       <S.InputWrapper>
         {!!icon && <S.Icon iconPosition={iconPosition}>{icon}</S.Icon>}
         <S.Input
@@ -44,7 +43,9 @@ const TextField = ({
           value={value}
           iconPosition={iconPosition}
           disabled={disabled}
-          {...rest}
+          name={name}
+          {...(label ? { id: name } : {})}
+          {...props}
         />
       </S.InputWrapper>
       {!!error && <S.Error>{error}</S.Error>}
