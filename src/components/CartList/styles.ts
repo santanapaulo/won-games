ea43287648928d73f294fe9982283500e4ac54fd
Paulo Santana (PH) <santana.pauloh@gmail.com>
2021-06-03 15:45:16 -0300

    CartDropdown e UserDropdown components

--- src/components/CartList/styles.ts
+++ src/components/CartList/styles.ts
@@ -1,13 +1,42 @@
 import { tint } from 'polished';
-import styled, { css } from 'styled-components';
+import styled, { css, DefaultTheme } from 'styled-components';
 import media from 'styled-media-query';
 
-export const Wrapper = styled.div`
-  ${({ theme }) => css`
+import * as EmptyStyles from 'components/Empty/styles';
+
+type WrapperProps = {
+  isEmpty: boolean;
+};
+
+const wrapperModifiers = {
+  isEmpty: (theme: DefaultTheme) => css`
+    ${EmptyStyles.Wrapper} {
+      padding-bottom: ${theme.spacings.medium};
+    }
+
+    ${EmptyStyles.Image} {
+      max-width: 20rem;
+    }
+
+    ${EmptyStyles.Title} {
+      font-size: ${theme.font.sizes.large};
+    }
+
+    ${EmptyStyles.Description} {
+      color: ${theme.colors.black};
+      font-size: ${theme.font.sizes.medium};
+    }
+  `,
+};
+
+export const Wrapper = styled.div<WrapperProps>`
+  ${({ theme, isEmpty }) => css`
     background: ${theme.colors.white};
     display: flex;
     flex-direction: column;
     align-self: start;
+
+    ${isEmpty && wrapperModifiers.isEmpty(theme)};
   `}
 `;
 
